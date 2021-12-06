5faab2ced34eee2aa3569707d61861685aa704e6
Damian Nolan <damiannolan@gmail.com>
2021-12-06 10:07:00 +0100

    updating hermes config and network scripts -  adding version output, override rosetta port

--- network/hermes/variables.sh
+++ network/hermes/variables.sh
@@ -1,6 +1,7 @@
  #!/bin/bash
-HERMES_BINARY=hermes
+HERMES_BINARY=/Users/damiannolan/development/cosmos/ibc-rs/target/debug/hermes
 HERMES_DIRECTORY=./network/hermes/
 CONFIG_DIR=./network/hermes/config.toml
 
-
+echo "Using hermes relayer version: "
+$HERMES_BINARY --version | sed 's/^/    /'
