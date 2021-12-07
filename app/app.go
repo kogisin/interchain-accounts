2f364fee689aea6cd959b05839427cc4bedca3b8
Damian Nolan <damiannolan@gmail.com>
2021-12-07 20:53:42 +0100

    removing irrelevant comment in app.go

--- app/app.go
+++ app/app.go
@@ -377,7 +377,7 @@ func New(
 	ibcRouter.AddRoute(icacontrollertypes.SubModuleName, icaControllerIBCModule).
 		AddRoute(icahosttypes.SubModuleName, icaHostIBCModule).
 		AddRoute(ibctransfertypes.ModuleName, transferIBCModule).
-		AddRoute(intertxtypes.ModuleName, icaControllerIBCModule) // ica with mock auth module stack route to ica (top level of middleware stack)
+		AddRoute(intertxtypes.ModuleName, icaControllerIBCModule)
 	app.IBCKeeper.SetRouter(ibcRouter)
 
 	// Create evidence Keeper for to register the IBC light client misbehaviour evidence route
